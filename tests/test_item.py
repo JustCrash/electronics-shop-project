0fc619c4506ee27001ad8ed2e13508d96aea0c2d
Artem <baza.a1@bk.ru>
2024-01-29 18:51:53 +0300

    Тесты для новых методов

--- tests/test_item.py
+++ tests/test_item.py
@@ -19,3 +19,24 @@ def test_apply_discount():
     item2.apply_discount()
     assert item1.price == 8400
     assert item2.price == 960
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('../src/items.csv')  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_name():
+    Item.name = 'Смартфон'
+    assert Item.name == 'Смартфон'
+    Item.name = 'СуперСмартфон'
+    assert Item.name == 'СуперСмартфон'
